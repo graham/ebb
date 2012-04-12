5433b2ed9b555cd7612c457a1d8f9c62dd06ab35
Graham Abbott <graham.abbott@gmail.com>
2012-04-12 00:31:37 -0700

    test all script to make testing the whole suite easier.

---
+++ test_all.py
@@ -0,0 +1,13 @@
+import unittest
+import json
+import sys
+import json
+
+import trees
+import ops
+
+from test_trees import *
+from test_ops import *
+
+if __name__ == '__main__':
+    unittest.main()
