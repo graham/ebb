129f834a8aa936ee06026975094d8594a960f810
Graham Abbott <graham.abbott@gmail.com>
2012-12-04 10:04:38 -0800

    making this a bit more flexible.

--- python/primal.py
+++ python/primal.py
@@ -1,49 +1,51 @@
 import math
+import sys
 
-def gen_primes():
-    def is_prime(n):
-        if n == 2:
-            return True
-
-        for i in range(3, int(math.sqrt(n))):
-            if n % i == 0:
-                return False
-
+def is_prime(n):
+    if n == 2:
         return True
 
-    def gen():
-        yield 2
-        n = 3
-        while True:
-            if is_prime(n):
-                yield n
-            n += 2
-                               
-    return gen()
+    for i in range(3, int(math.sqrt(n))):
+        if n % i == 0:
+            return False
 
+    return True
 
-class Doc(object):
+def gen_next_prime(prev):
+    prev += 2
+    while not is_prime(prev):
+        prev += 2
+    return prev
+
+class Revision(object):
     def __init__(self):
-        self.p = gen_primes()
-        self.pvis = 1
+        self.prev = 3
+        self.current_revision = 2
 
     def __repr__(self):
-        return "<Doc rev: %i>" % self.pvis
+        return "<Revision %i>" % sys.getsizeof(self.current_revision)
 
-    def inject_version(self, id):
-        self.pvis *= id
-    
     def next_rev(self):
-        while True:
-            n = self.p.next()
-            if not self.is_child_of(n):
-                self.pvis *= n
-                return
+        self.prev = gen_next_prime(self.prev)
+        return self.prev
 
-    def is_child_of(self, id):
-        if self.pvis % id == 0:
+    def include(self, revision):
+        if self.current_revision % revision != 0:
+            self.current_revision *= revision
             return True
         else:
             return False
 
+    def exclude(self, revision):
+        if self.current_revision % revision == 0:
+            self.current_revision /= revision
+            return True
+        else:
+            return False
 
+    def test(self, revision):
+        if self.current_revision % revision == 0:
+            return True
+        else:
+            return False
+    
